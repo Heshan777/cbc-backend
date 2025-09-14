608a2a9c2998382e558d7b87e8edfd2a39010d67
Heshan777 <sachinthaweerakoon81@gmail.com>
2025-09-14 10:58:28 +0530

    is customer function create for validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,7 +72,15 @@ export function isAdmin(req){
     }
     return true;
 }
-        
+
+export function isCustomer(req){
+    if(req.user== null){
+        return false;    }
+ if(req.user.role!='user'){
+    return false;
+}
+return true;
+}    
 
 
 
