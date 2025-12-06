16140cafc936a7de6c71d7221788c803c5a2b883
Heshan777 <sachinthaweerakoon81@gmail.com>
2025-12-06 22:30:20 +0530

    supabase fixed

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -6,4 +6,12 @@ const orderRouter = express.Router();
 orderRouter.post("/", createOrder)
 orderRouter.get("/",getOrders)
 orderRouter.put("/status/:orderID",updateOrderStatus)
+
+// --- In your Backend's Order Routes file ---
+
+// This is the new endpoint you need to create:
+// GET /api/orders/my-orders
+// (Requires user to be logged in)
+
+
 export default orderRouter;
